afbf6bb861eb63f685f63409c8860dcce59f9e9c
xerexcoded <arnavbanerjee61@gmail.com>
2021-08-24 00:44:23 +0530

    changes

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -122,9 +122,9 @@ export const getStaticPaths = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
 
-  const paths = countries.map((country) => ({
+  const paths = countries.map((country,index) => ({
    params: {  id: country.alpha3Code },
-   
+   key : {index}
   }));
   
   return {
